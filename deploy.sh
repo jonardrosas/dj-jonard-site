83431cc9d9c34d0f44119bc80f6a486530807c3e
Jonard <jonardrosas@gmail.com>
2025-11-23 14:51:13 +0800

    add deploy.sh

---
+++ deploy.sh
@@ -0,0 +1,4 @@
+# copy to devops/systemd/jonard-backend.service /etc/systemd/systemd
+# systemctl daemon-reload
+# sudo systemctl enable jonard-backend.service
+# sudo systemctl start my-custom-service.service
